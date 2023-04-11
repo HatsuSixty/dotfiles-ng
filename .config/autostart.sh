a02ae94fa3c0a6f7e2d22aa6f3786fd4589b1351
robertosixty1 <roberto011herminegildo@gmail.com>
2023-04-10 22:34:00 -0300

    Set gtk theme on startup

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -11,3 +11,8 @@ run dunst
 run pulseaudio --start
 run fcitx-autostart
 run udiskie
+
+gnome_schema=org.gnome.desktop.interface
+
+gsettings set $gnome_schema gtk-theme 'Qogir-Dark'
+gsettings set $gnome_schema icon-theme 'Qogir-Dark'
