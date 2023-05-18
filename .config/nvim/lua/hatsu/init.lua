7d25cc0df7cde634cb5b92f3f65fab39b38a3ea4
robertosixty1 <roberto011herminegildo@gmail.com>
2023-05-18 13:16:10 -0300

    Add my neovim config

---
+++ .config/nvim/lua/hatsu/init.lua
@@ -0,0 +1,8 @@
+require("hatsu.remap")
+require("hatsu.packer")
+require("hatsu.set")  
+  
+  
+  
+  
+  
---
+++ .config/nvim/lua/hatsu/packer.lua
@@ -0,0 +1,57 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use {
+    'catppuccin/nvim', as = 'catppuccin',
+    config = function()
+      vim.cmd('colorscheme catppuccin')
+    end
+  }
+
+  use {
+    -- recommended packer way of installing it is to run this function, copied from documentation
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+
+  use 'theprimeagen/harpoon'
+  use 'mbbill/undotree'
+  use 'tpope/vim-fugitive'
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+ 
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+end)
