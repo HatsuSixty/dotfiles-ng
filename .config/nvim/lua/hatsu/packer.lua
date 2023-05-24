8c1a3032b9bf7e8a4e1cad77f049156e63bc132e
robertosixty1 <roberto011herminegildo@gmail.com>
2023-05-24 12:56:32 -0300

    Add `vim-be-good` to neovim

--- .config/nvim/lua/hatsu/packer.lua
+++ .config/nvim/lua/hatsu/packer.lua
@@ -31,6 +31,7 @@ return require('packer').startup(function(use)
   use 'theprimeagen/harpoon'
   use 'mbbill/undotree'
   use 'tpope/vim-fugitive'
+  use 'theprimeagen/vim-be-good'
 
   use {
     'VonHeikemen/lsp-zero.nvim',
