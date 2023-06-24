83d7302e74519b98358768e6a7974b34402abb4a
HatsuSixty <roberto011herminegildo@gmail.com>
2023-06-24 18:27:13 -0300

    Don't explicitly set cursor theme in Hyprland

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -16,7 +16,6 @@ run dex -a
 gnome_schema=org.gnome.desktop.interface
 cursor_theme=Adwaita-smol
 
-hyprctl setcursor $cursor_theme 24
 gsettings set $gnome_schema gtk-theme 'adw-gtk3-dark'
 gsettings set $gnome_schema icon-theme 'Adwaita'
 gsettings set $gnome_schema cursor-theme $cursor_theme
