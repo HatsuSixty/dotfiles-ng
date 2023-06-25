d7c457d0c6a1382aa130b3e50f79fde125103bb1
HatsuSixty <roberto011herminegildo@gmail.com>
2023-06-25 20:47:21 -0300

    I don't even remember why i was setting my theme in `autostart.sh`

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -12,10 +12,3 @@ run pipewire-pulse 1>&2 > /dev/null
 run fcitx-autostart
 run udiskie
 run dex -a
-
-gnome_schema=org.gnome.desktop.interface
-cursor_theme=Adwaita-smol
-
-gsettings set $gnome_schema gtk-theme 'Dracula'
-gsettings set $gnome_schema icon-theme 'Adwaita'
-gsettings set $gnome_schema cursor-theme $cursor_theme
