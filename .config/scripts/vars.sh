2e3f76045385cd93d93f40373d1056a31174123d
HatsuSixty <roberto011herminegildo@gmail.com>
2023-06-28 15:09:21 -0300

    Update `bemenu` options

--- .config/scripts/vars.sh
+++ .config/scripts/vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export TERMINAL=alacritty
-export BEMENU_OPTS='--tb #45007a --tf #ffffff --hb #45007a --hf #ffffff -H 24'
+export BEMENU_OPTS='-fn "FuraMono Nerd Font Mono" -c -B 2 -R 10 -M 350 --fixed-height -l 12 -H 27 --fb #120026 --ab #120026 --nb #120026 --tb #45007a --tf #ffffff --hb #45007a --hf #ffffff --bdr #45007a'
