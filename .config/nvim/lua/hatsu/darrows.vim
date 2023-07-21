6126b8d69f1513dba3eb245f6e43b2faf45680a7
HatsuSixty <roberto011herminegildo@gmail.com>
2023-07-20 21:10:04 -0300

    Disable arrow keys in neovim

---
+++ .config/nvim/lua/hatsu/darrows.vim
@@ -0,0 +1,4 @@
+nnoremap <Left>  :echoe "Use h"<CR>
+nnoremap <Right> :echoe "Use l"<CR>
+nnoremap <Up>    :echoe "Use k"<CR>
+nnoremap <Down>  :echoe "Use j"<CR>
--- .config/nvim/lua/hatsu/init.lua
+++ .config/nvim/lua/hatsu/init.lua
@@ -1,8 +1,7 @@
 require("hatsu.remap")
 require("hatsu.packer")
-require("hatsu.set")  
-  
-  
-  
-  
-  
+require("hatsu.set")
+
+local nvimrc = os.getenv("HOME") .. "/.config/nvim"
+
+vim.cmd('source ' .. nvimrc .. '/lua/hatsu/darrows.vim')
