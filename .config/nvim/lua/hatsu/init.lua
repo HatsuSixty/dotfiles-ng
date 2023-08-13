01708ea167ff964a36addee88cd44b1c183efb2b
HatsuSixty <hatsunotsus@gmail.com>
2023-08-13 14:06:52 -0300

    `hjkl` sucks, change my mind

--- .config/nvim/lua/hatsu/init.lua
+++ .config/nvim/lua/hatsu/init.lua
@@ -1,7 +1,3 @@
 require("hatsu.remap")
 require("hatsu.packer")
 require("hatsu.set")
-
-local nvimrc = os.getenv("HOME") .. "/.config/nvim"
-
-vim.cmd('source ' .. nvimrc .. '/lua/hatsu/darrows.vim')
