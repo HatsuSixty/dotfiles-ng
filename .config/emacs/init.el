e6a4d9dba6cfb629e986bc3c493751905ce0b801
HatsuSixty <hatsunotsus@gmail.com>
2023-09-22 13:51:03 -0300

    Change emacs' theme

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -4,7 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(zenburn))
+ '(custom-enabled-themes '(gruber-darker))
  '(package-selected-packages
    '(csharp-mode lua-mode markdown-mode magit rust-mode nasm-mode gruber-darker-theme yaml-mode yasnippet-snippets gdscript-mode company multiple-cursors)))
 (custom-set-faces
