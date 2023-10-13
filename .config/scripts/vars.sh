45dfb8767fcb09310ffe4e491ddfd105deb608d7
HatsuSixty <hatsunotsus@gmail.com>
2023-10-13 12:55:27 -0300

    Change terminal to `xfce4-terminal`

--- .config/scripts/vars.sh
+++ .config/scripts/vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export TERMINAL=konsole
+export TERMINAL=xfce4-terminal
 export BEMENU_OPTS='-fn "FuraMono Nerd Font Mono" -c -B 2 -R 10 -M 350 --fixed-height -l 12 -H 27 --fb #120026 --ab #120026 --nb #120026 --tb #45007a --tf #ffffff --hb #45007a --hf #ffffff --bdr #45007a'
