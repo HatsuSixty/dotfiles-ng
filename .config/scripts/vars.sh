c1122c321c9fb4aae8668dc5b18e7ba251e05bbd
HatsuSixty <hatsunotsus@gmail.com>
2023-10-17 21:28:17 -0300

    Update `bemenu` config again bcz i have nothing better to do

--- .config/scripts/vars.sh
+++ .config/scripts/vars.sh
@@ -2,4 +2,4 @@
 
 export TERMINAL=alacritty
 #export BEMENU_OPTS='-fn "FuraMono Nerd Font Mono" -c -B 2 -R 10 -M 350 --fixed-height -l 12 -H 27 --fb #120026 --ab #120026 --nb #120026 --tb #45007a --tf #ffffff --hb #45007a --hf #ffffff --bdr #45007a'
-export BEMENU_OPTS='-fn "FuraMono Nerd Font Mono" -H 20 --fb #222222 --tb #285577 --tf #ffffff --hf #ffffff --hb #285577 --cw 2 --hp 8'
+export BEMENU_OPTS='-fn "FuraMono Nerd Font Mono" -H 20 --fb #222222 --nb #222222 --ab #222222 --tb #285577 --tf #ffffff --hf #ffffff --hb #285577 --cw 2 --hp 8'
